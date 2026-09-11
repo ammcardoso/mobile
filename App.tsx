5e12e5633d51599bb832726e40d33bff704b174f
Adriana Cardoso <adrianamirianmc@gmail.com>
2026-09-11 16:50:08 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-
-import AppLoading from 'expo-app-loading';
+import React, { useCallback, useEffect, useState } from 'react';
+import { View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 
 import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
 import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
 
 import AppStack from './src/routes/AppStack';
 
+SplashScreen.preventAutoHideAsync().catch(() => {});
+
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
   let [fontsLoaded] = useFonts({
     Archivo_400Regular,
     Archivo_700Bold,
@@ -16,14 +20,33 @@ export default function App() {
     Poppins_600SemiBold
   });
 
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  } else {
-    return (
-      <>
-        <AppStack />
-        <StatusBar style="light" />
-      </>
-    );
+  useEffect(() => {
+    async function prepare() {
+      if (fontsLoaded) {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, [fontsLoaded]);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      try {
+        await SplashScreen.hideAsync();
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
   }
+
+  return (
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <AppStack />
+      <StatusBar style="light" />
+    </View>
+  );
 }
